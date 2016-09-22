f47b577bb44c2654648293a0b7a05943253fa2dc
JayJay <bricksoftapps@gmail.com>
2016-09-22 03:34:46 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var del = require('del');
+var ncp = require('ncp').ncp;
+
+var init_Haxe = {};
+
+init_Haxe = (function(){}).prototype = {
+	install : function (directory){
+		var source = __dirname + "/template";
+		var destination = directory ? (directory == "." ? process.cwd() : directory) : process.cwd();
+		ncp(source, destination, function (err) {
+			if (err) {
+				return console.error(err);
+			}
+			del([
+				destination+"**/.git*",
+				destination+"build/*/*",
+				destination+"targets/*/*",
+				destination+"targets/#*"
+			]).then( () => {
+				console.log("Template successfully installed in " + destination);
+			});
+		});
+	}
+};
+
+module.exports = init_Haxe;
